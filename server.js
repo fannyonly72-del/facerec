7b34c9646ee245cae2de31b9ad00e4b79b63053c
fannyonly72-del <fannyonly72@gmail.com>
2026-03-18 15:02:33 +0900

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' })); // Increase limit to 10MB
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // MongoDB connection
 const uri = "mongodb+srv://hiraiginjialt4_db_user:l2CDwnX4pQdcEvI7@cluster0.sjtehir.mongodb.net/attendance_system?retryWrites=true&w=majority";
